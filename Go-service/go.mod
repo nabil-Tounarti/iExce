0426b30adb7c8094cb8ddecc951a58fcbdb05cc0
NABIL <nabil.tounarti0@gmail.com>
2024-05-19 00:59:53 +0200

    go-service

---
+++ Go-service/go.mod
@@ -0,0 +1,39 @@
+module Go-service
+
+go 1.22.3
+
+require (
+	github.com/Shopify/sarama v1.34.1
+	go.mongodb.org/mongo-driver v1.15.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/eapache/go-resiliency v1.2.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20180814174437-776d5712da21 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.2 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.0.0 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.2 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/klauspost/compress v1.15.6 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/pierrec/lz4/v4 v4.1.14 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
+
+replace github.com/Shopify/sarama => github.com/IBM/sarama v1.34.1
---
+++ Go-service/main.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/Shopify/sarama"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	kafkaTopic   = "HachCode_Topic"
+	kafkaBrokers = "kafka-server:9092" // Update if needed
+	mongoURI     = "mongodb://mongodb:27017"
+	mongoDBName  = "Go_db"
+	mongoColl    = "Go_collection"
+)
+
+func main() {
+	// Set up a connection to MongoDB
+	clientOptions := options.Client().ApplyURI(mongoURI)
+	mongoClient, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+	defer mongoClient.Disconnect(context.TODO())
+
+	collection := mongoClient.Database(mongoDBName).Collection(mongoColl)
+
+	// Set up a new Sarama consumer group
+	config := sarama.NewConfig()
+	config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRoundRobin
+	config.Version = sarama.V2_5_0_0 // Set the appropriate Kafka version
+
+	consumerGroup, err := sarama.NewConsumerGroup([]string{kafkaBrokers}, "my-group", config)
+	if err != nil {
+		log.Fatalf("Failed to create consumer group: %v", err)
+	}
+	defer consumerGroup.Close()
+
+	// Set up a channel to listen for OS signals
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	// Start HTTP server for API
+	http.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
+		// Query MongoDB for data
+		cur, err := collection.Find(context.Background(), bson.D{})
+		if err != nil {
+			http.Error(w, "Failed to query MongoDB", http.StatusInternalServerError)
+			return
+		}
+		defer cur.Close(context.Background())
+
+		var data []bson.M
+		if err := cur.All(context.Background(), &data); err != nil {
+			http.Error(w, "Failed to decode MongoDB documents", http.StatusInternalServerError)
+			return
+		}
+
+		// Convert data to JSON and write to response
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(data)
+	})
+
+	go func() {
+		log.Fatal(http.ListenAndServe(":3336", nil))
+	}()
+
+	consumer := Consumer{
+		ready:      make(chan bool),
+		collection: collection,
+	}
+
+	ctx := context.Background()
+	go func() {
+		for {
+			if err := consumerGroup.Consume(ctx, []string{kafkaTopic}, &consumer); err != nil {
+				log.Fatalf("Error from consumer: %v", err)
+			}
+			// Check if context was cancelled, signaling that the consumer should stop
+			if ctx.Err() != nil {
+				return
+			}
+			consumer.ready = make(chan bool) // Reinitialize ready channel
+		}
+	}()
+
+	<-consumer.ready // Await until the consumer has been set up
+	log.Println("Sarama consumer up and running!")
+
+	<-signals // Wait for a termination signal
+
+	log.Println("Terminating...")
+}
+
+type Consumer struct {
+	ready      chan bool
+	collection *mongo.Collection
+}
+
+// Setup is run at the beginning of a new session, before ConsumeClaim
+func (consumer *Consumer) Setup(_ sarama.ConsumerGroupSession) error {
+	// Mark the consumer as ready
+	close(consumer.ready)
+	return nil
+}
+
+// Cleanup is run at the end of a session, once all ConsumeClaim goroutines have exited
+func (consumer *Consumer) Cleanup(_ sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+// ConsumeClaim must start a consumer loop of ConsumerGroupClaim's Messages().
+func (consumer *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for message := range claim.Messages() {
+		log.Printf("Message claimed: value = %s, timestamp = %v, topic = %s", string(message.Value), message.Timestamp, message.Topic)
+
+		// Insert the message into MongoDB
+		_, err := consumer.collection.InsertOne(context.TODO(), bson.M{
+			"topic":     message.Topic,
+			"partition": message.Partition,
+			"offset":    message.Offset,
+			"key":       string(message.Key),
+			"value":     string(message.Value),
+			"timestamp": message.Timestamp,
+		})
+		if err != nil {
+			log.Printf("Failed to insert message into MongoDB: %v", err)
+		}
+
+		session.MarkMessage(message, "")
+	}
+	return nil
+}
