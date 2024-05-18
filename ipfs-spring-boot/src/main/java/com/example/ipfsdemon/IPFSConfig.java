69b295f0dbff72b256d1c70d62dbf75e6af4c441
NABIL <nabil.tounarti0@gmail.com>
2024-05-18 16:57:24 +0200

    spring boot api

--- ipfs-spring-boot/src/main/java/com/example/ipfsdemon/IPFSConfig.java
+++ ipfs-spring-boot/src/main/java/com/example/ipfsdemon/IPFSConfig.java
@@ -1,6 +1,7 @@
 package com.example.ipfsdemon;
 
 import io.ipfs.api.IPFS;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -8,11 +9,10 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class IPFSConfig {
-
     IPFS ipfs;
 
     public IPFSConfig() {
-        ipfs = new IPFS("/ip4/127.0.0.1/tcp/5001");
+        ipfs = new IPFS("ipfs-daemon", 5001);
     }
 
 }
--- ipfs-spring-boot/src/main/java/com/example/ipfsdemon/kafka/KafkaProducerConfig.java
+++ ipfs-spring-boot/src/main/java/com/example/ipfsdemon/kafka/KafkaProducerConfig.java
@@ -8,7 +8,7 @@ import java.util.Properties;
 public class KafkaProducerConfig {
     public Properties getProducerProperties() {
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); // Kafka broker address
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-server:9092"); // Kafka broker address
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         return props;
